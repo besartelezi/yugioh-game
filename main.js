fadaa4b65c539bb5c6c6b7e54bb4858f23f37cef
Besart Elezi <besart.elezi@hotmail.com>
2022-07-09 22:49:38 +0200

    changed requires, replaced js for tsc

---
+++ main.js
@@ -0,0 +1,12 @@
+import {Card} from "./typescripts/Card";
+import {MonsterCard} from "./typescripts/MonsterCard";
+
+const BesartTheCreator = new MonsterCard("Besart The Creator",
+    "/images/shrek.png",
+    "The Creator of this cruel, cruel game.",
+    false,
+    6900, 4200,
+    false, false,
+    4);
+
+console.log(BesartTheCreator)
